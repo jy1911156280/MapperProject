a4aa790e935e7bc0ef8cbe7ac322989243cffec9
liwenqi <40755979+15936040776@users.noreply.github.com>
2020-05-19 19:35:23 +0800

    代码提交

---
+++ mapping-qy108-management/mapping-qy108-mapper/src/main/java/com/aaa/six/mapper/LoginLogsMapper.java
@@ -0,0 +1,7 @@
+package com.aaa.six.mapper;
+
+import com.aaa.six.model.LoginLogs;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface LoginLogsMapper extends Mapper<LoginLogs> {
+}
--- mapping-qy108-provider-management/mapping-qy108-system-provider-6081/src/main/java/com/aaa/six/controller/LoginController.java
+++ mapping-qy108-provider-management/mapping-qy108-system-provider-6081/src/main/java/com/aaa/six/controller/LoginController.java
@@ -2,12 +2,15 @@ package com.aaa.six.controller;
 
 import com.aaa.six.model.User;
 import com.aaa.six.redis.RedisService;
+import com.aaa.six.service.LoginLogsService;
 import com.aaa.six.service.LoginService;
 import com.aaa.six.vo.TokenVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.UnknownHostException;
 
 /**
  * @program: mapping-qy108
@@ -23,17 +26,31 @@ public class LoginController {
 
     @Autowired
     private LoginService loginService;
+
+    @Autowired
+    private LoginLogsService loginLogsService;
+    
     /**
-     * @author lwq
+     * @author lwq 
      * @description
-     *          执行登录操作
+     *    执行登录操作
      * @param: [user]
-     * @date 2020/5/16
+     * @date 2020/5/19
      * @return com.aaa.six.vo.TokenVo
-     * @throws
+     * @throws 
      **/
     @PostMapping("/doLogin")
-    public TokenVo doLogin(@RequestBody User user) {
-        return loginService.doLogin(user, redisService);
+    public TokenVo doLogin(@RequestBody User user){
+        TokenVo tokenVo = loginService.doLogin(user, redisService);
+        try {
+            if ("true".equals(tokenVo.getIfSuccess().toString())){
+                loginLogsService.doLoginLogs(user.getUsername());
+            }
+        }catch (UnknownHostException u){
+            u.printStackTrace();
+            System.out.println("系统正在维护，请稍后再试");
+        }
+        return tokenVo;
     }
 }
+
