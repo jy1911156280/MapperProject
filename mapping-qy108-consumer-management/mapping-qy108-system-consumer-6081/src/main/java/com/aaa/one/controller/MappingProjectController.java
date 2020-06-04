74bb1eae3640aa8e10444bc066c7fa88b5c2349e
jy1911156280 <1911156280@qq.com>
2020-06-04 21:52:03 +0800

    代码提交

--- mapping-qy108-consumer-management/mapping-qy108-system-consumer-6081/src/main/java/com/aaa/one/controller/MappingProjectController.java
+++ mapping-qy108-consumer-management/mapping-qy108-system-consumer-6081/src/main/java/com/aaa/one/controller/MappingProjectController.java
@@ -1,150 +1,129 @@
-//package com.aaa.one.controller;
-//
-//import com.aaa.one.IQYService;
-//import com.aaa.one.base.BaseController;
-//import com.aaa.one.base.ResultData;
-//import com.aaa.one.model.MappingProject;
-//import com.github.pagehelper.PageInfo;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-///**
-// * @ProjectName: mapping-qy108
-// * @Package: com.aaa.six.controller
-// * @ClassName: MappingProjectController
-// * @Author: lifuju
-// * @Description:
-// * @Date: 2020/5/21 11:42
-// * @Version: 1.0
-// */
-//@RestController
-//@Api(value = "项目信息", tags = "项目管理板块")
-//public class MappingProjectController extends BaseController{
-//    @Autowired
-//    private IQYService qyService;
-//
-//
-//    /**
-//     *@Description: TODO
-//     * 项目管理 新增方法 单个新增
-//     *@Param :  [mappingProject]
-//     *@MethodName: add
-//     *@Author: lifuju
-//     *@Date: 2020/5/22 15:48
-//     *@Return: com.aaa.six.base.ResultData
-//     */
-//    @PostMapping("/addMappingProject")
-//    @ApiOperation(value = "项目管理", notes = "新增一条数据")
-//    public ResultData add(@RequestBody MappingProject mappingProject){
-//        Integer i = qyService.add(mappingProject);
-//        if (i!= null && i!=0) {
-//            return ADDSuccess();
-//        }
-//        return ADDFailed();
-//    }
-//    /**
-//     *@Description: TODO
-//     *项目管理 删除方法  单个删除
-//     *@Param :  [id]
-//     *@MethodName: delMappingProject
-//     *@Author: lifuju
-//     *@Date: 2020/5/22 21:47
-//     *@Return: com.aaa.six.base.ResultData
-//     */
-//    @GetMapping("/delMappingProject/{id}")
-//    @ApiOperation(value = "项目管理", notes = "删除一条数据")
-//    public ResultData delMappingProject(@PathVariable("id")Long id){
-//        Integer integer = qyService.delMappingProject(id);
-//        if (integer!= null && integer!=0) {
-//            return deleteSuccess();
-//        }
-//        return deleteFailed();
-//    }
-//
-//    /**
-//     *@Description: TODO
-//     * 项目管理 删除方法 批量删除
-//     *@Param :  [ids]
-//     *@MethodName: delBatch
-//     *@Author: lifuju
-//     *@Date: 2020/5/24 8:57
-//     *@Return: com.aaa.six.base.ResultData
-//     */
-//    @PostMapping("/deleteBatchMappingProject")
-//    @ApiOperation(value = "项目管理", notes = "批量删除数据")
-//    public ResultData delBatch(@RequestBody List<Object> ids) throws  Exception{
-//        Integer integer = qyService.delBatch(ids);
-//        if (integer>0) {
-//            return deleteSuccess();
-//        }
-//        return deleteFailed();
-//    }
-//
-//    /**
-//     *@Description: TODO
-//     * 项目管理 查询方法 单个查询
-//     *@Param :  [id]
-//     *@MethodName: selectOne
-//     *@Author: lifuju
-//     *@Date: 2020/5/24 8:58
-//     *@Return: com.aaa.six.base.ResultData
-//     */
-//
-//    @GetMapping("/selectOne")
-//    @ApiOperation(value = "项目管理", notes = "查询一条数据")
-//    public ResultData selectOne(@RequestParam("id")Long id){
-//
-//        MappingProject mappingProject = qyService.selectOne(id);
-//        if (null != mappingProject&& !"".equals(mappingProject)) {
-//            return selectSuccess(mappingProject);
-//        }
-//        return selectFailed();
-//    }
-//
-//    /**
-//     *@Description: TODO
-//     * 项目管理 更新方法 单个更新
-//     *@Param :  [mappingProject]
-//     *@MethodName: update
-//     *@Author: lifuju
-//     *@Date: 2020/5/24 8:58
-//     *@Return: com.aaa.six.base.ResultData
-//     */
-//
-//    @PostMapping("/updateMappingProject")
-//    @ApiOperation(value = "项目管理", notes = "更新一条数据")
-//    ResultData update(@RequestBody MappingProject mappingProject){
-//        Integer integer = qyService.update(mappingProject);
-//        if (integer!=null && integer!=0){
-//            return updateSuccess();
-//        }
-//        return updateFailed();
-//    }
-//
-//    /**
-//     *@Description: TODO
-//     * 项目管理 查询方法 分页查询
-//     *@Param :  [pageNo, pageSize]
-//     *@MethodName: queryListByPage
-//     *@Author: lifuju
-//     *@Date: 2020/5/24 8:59
-//     *@Return: com.aaa.six.base.ResultData
-//     */
-//    @PostMapping("/mappingProjectByPage")
-//    @ApiOperation(value = "项目管理", notes = "项目管理分页查询")
-//    public ResultData queryListByPage(@RequestParam("pageNo") Integer pageNo, @RequestParam("pageSize") Integer pageSize){
-//        PageInfo mappingProjectPageInfo = qyService.queryListByPage(pageNo, pageSize);
-//        if (null!=mappingProjectPageInfo) {
-//            return selectSuccess(mappingProjectPageInfo);
-//        }
-//        return selectFailed();
-//
-//    }
-//
-//
-//
-//}
+package com.aaa.one.controller;
+
+import com.aaa.one.IQYService;
+import com.aaa.one.base.BaseController;
+import com.aaa.one.base.ResultData;
+import com.aaa.one.model.MappingProject;
+import com.aaa.one.model.MappingUnit;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@Api(value = "项目信息", tags = "项目管理板块")
+public class MappingProjectController extends BaseController{
+    @Autowired
+    private IQYService iqyService;
+
+    /**
+     *@Author Jiayi
+     *@Description       根据项目类型查询项目 开启分页
+     *@Param [where, currentPage, pageSize]
+     *@Date 2020/6/3 14:07
+     *@return com.github.pagehelper.PageInfo<com.aaa.one.model.MappingProject>
+     **/
+    @GetMapping("queryListByTypeAndPage")
+    PageInfo<MappingProject> queryListByTypeAndPage(@RequestParam("where") String where, @RequestParam("currentPage") Integer currentPage, @RequestParam("pageSize") Integer pageSize){
+        return iqyService.queryListByTypeAndPage(where,currentPage,pageSize);
+    }
+
+
+    /**
+     *@Author Jiayi
+     *@Description   添加项目
+     *@Param [mappingProject]
+     *@Date 2020/6/3 13:54
+     *@return boolean
+     **/
+    @PostMapping("/addMappingProject")
+    boolean addMappingProject(@RequestBody MappingProject mappingProject){
+        return iqyService.addMappingProject(mappingProject);
+    }
+
+    /**
+     *@Author Jiayi
+     *@Description   查询所有项目
+     *@Param []
+     *@Date 2020/6/3 14:01
+     *@return java.util.List<com.aaa.one.model.MappingProject>
+     **/
+    @GetMapping("/queryListAllProject")
+    List<MappingProject> queryListAllProject(){
+        return iqyService.queryListAllProject();
+    }
+
+    /**
+     *@Author Jiayi
+     *@Description       查询所有项目 开启分页
+     *@Param [currentPage, pageSize]
+     *@Date 2020/6/3 14:04
+     *@return com.github.pagehelper.PageInfo<com.aaa.one.model.MappingProject>
+     **/
+    @GetMapping("/queryListAllByPage")
+    PageInfo<MappingProject> queryListAllByPage(@RequestParam("currentPage")  Integer currentPage, @RequestParam("pageSize") Integer pageSize){
+        return iqyService.queryListAllByPage(currentPage,pageSize);
+    }
+
+    /**
+     *@Author Jiayi
+     *@Description   修改项目信息
+     *@Param [mappingProject]
+     *@Date 2020/6/3 14:08
+     *@return java.lang.Boolean
+     **/
+    @PutMapping("/updateProject")
+    Boolean updateProject(@RequestBody MappingProject mappingProject){
+        return iqyService.updateProject(mappingProject);
+    }
+
+    /**
+     *@Author Jiayi
+     *@Description   删除项目
+     *@Param [mappingProject]
+     *@Date 2020/6/3 14:09
+     *@return java.lang.Boolean
+     **/
+    @DeleteMapping("/deleteProject")
+    Boolean deleteProject(@RequestBody MappingProject mappingProject){
+        return iqyService.deleteProject(mappingProject);
+    }
+    /**
+     *@Author Jiayi
+     *@Description   项目统计信息
+     *@Param []
+     *@Date 2020/6/3 14:09
+     *@return java.util.List<java.util.Map<java.lang.String,java.lang.Object>>
+     **/
+    @GetMapping("/projectTypeStatistics")
+    List<Map<String,Object>> projectTypeStatistics(){
+        return iqyService.projectTypeStatistics();
+    }
+
+
+
+
+
+    /**
+     *@Author Jiayi
+     *@Description  根据项目类型查询项目
+     *@Param [where]
+     *@Date 2020/6/2 11:35
+     *@return java.util.List<com.aaa.one.model.MappingProject>
+     **/
+    @GetMapping("/queryListByType")
+    List<MappingProject> queryListByType(@RequestParam("where") String where){
+        return iqyService.queryListByType(where);
+    }
+
+
+
+
+
+
+}
--- mapping-qy108-consumer-management/mapping-qy108-system-consumer-6081/src/main/java/com/aaa/one/controller/MappingUnitController.java
+++ mapping-qy108-consumer-management/mapping-qy108-system-consumer-6081/src/main/java/com/aaa/one/controller/MappingUnitController.java
@@ -2,8 +2,10 @@ package com.aaa.one.controller;
 
 import com.aaa.one.IQYService;
 import com.aaa.one.model.MappingUnit;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -44,4 +46,42 @@ public class MappingUnitController {
     Map<String,Object> getUnitLevel(){
         return iqyService.getUnitLevel();
     }
+
+
+
+    /**
+     *@Author Jiayi
+     *@Description   查询所有单位信息
+     *@Param []
+     *@Date 2020/6/4 9:39
+     *@return java.util.List<com.aaa.one.model.MappingUnit>
+     **/
+    @GetMapping("/queryListAllUnit")
+    List<MappingUnit> queryListAllUnit(){
+        return iqyService.queryListAllUnit();
+    }
+
+    /**
+     *@Author Jiayi
+     *@Description   查询所有单位信息    开启分页
+     *@Param [currentPage, pageSize]
+     *@Date 2020/6/4 9:41
+     *@return com.github.pagehelper.PageInfo<com.aaa.one.model.MappingUnit>
+     **/
+    @GetMapping("/queryListAllUnitByPage")
+    PageInfo<MappingUnit> queryListAllUnitByPage(@RequestParam("currentPage") Integer currentPage, @RequestParam("pageSize") Integer pageSize){
+        return iqyService.queryListAllUnitByPage(currentPage,pageSize);
+    }
+
+    /**
+     *@Author Jiayi
+     *@Description   人员设备统计查询
+     *@Param []
+     *@Date 2020/6/4 9:42
+     *@return java.util.List<com.aaa.one.model.TecUnitEptVo>
+     **/
+//    @GetMapping("/getAllInfoUnit")
+//     List<TecUnitEptVo> getAllInfo();
+
+
 }
